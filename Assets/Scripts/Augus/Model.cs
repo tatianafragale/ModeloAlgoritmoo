46ed6e05e23ab5fc70c7cfbfcadf3f3e551a29e0
Santax19 <santielcapo1@gmail.com>
2024-09-24 15:58:51 -0300

    updated player speed

--- Assets/Scripts/Augus/Model.cs
+++ Assets/Scripts/Augus/Model.cs
@@ -9,6 +9,6 @@ public class Model: MonoBehaviour
     // Método para obtener la posición del jugador
     public Vector3 GetMovement(Vector3 direction)
     {
-        return direction * speed * 1000 * Time.deltaTime;
+        return direction * speed * 2000 * Time.deltaTime;
     }
 }
